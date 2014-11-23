473055fdfdfe34d37fc9e5603508bc0543e14b3c
Roman Nestertsov <r.nestertsov@gmail.com>
2014-11-23 08:30:10 +0200

    Deleted dedicated yasnippet config

--- conf/conf-yasnippet.el
+++
@@ -1,9 +0,0 @@
-;;; yasnippet.el ---
-
-;; Configures yasnippet
-
-(require 'yasnippet)
-(setq yas/snippet-dirs (append '("~/.emacs.d/snippets/") yas/snippet-dirs))
-(yas/global-mode 1)
-
-(add-to-list 'auto-mode-alist '("/snippets/" . snippet-mode))
