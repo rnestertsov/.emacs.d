337d0a13043038dcbd34fa6b3d7ac0ca32f78594
Roman Nestertsov <r.nestertsov@gmail.com>
2014-11-23 08:47:28 +0200

    Start bookmarks buffer at startup

--- conf/misc.el
+++ conf/misc.el
@@ -7,4 +7,10 @@
 (setq create-lockfiles nil)
 
 ;; open *scratch* buffer at startup
-(setq inhibit-startup-message t)
+;; (setq inhibit-startup-message t)
+
+;; open *bookmarks* buffer at startup
+(setq inhibit-splash-screen t)
+(require 'bookmark)
+(bookmark-bmenu-list)
+(switch-to-buffer "*Bookmark List*")
